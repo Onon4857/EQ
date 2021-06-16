79aa21844e2aaec9c389000cdaaa65f278c4c8ed
002 <a002@002s-MacBook-Air.local>
2021-06-16 09:54:39 +0800

    Postcss tohiruulga

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    require("postcss-nested"),
+    require("autoprefixer"),
+    require("postcss-import"),
+    require("postcss-mixins"),
+    require("postcss-normalize"),
+  ],
+};
